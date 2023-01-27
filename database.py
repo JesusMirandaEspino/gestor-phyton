860aa3675610990e2ce149fb0c6c80c19c1e6e7d
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-01-27 08:40:03 -0600

    init test part 3

--- database.py
+++ database.py
@@ -1,13 +1,15 @@
 class Cliente:
+
     def __init__(self, dni, nombre, apellido):
         self.dni = dni
         self.nombre = nombre
         self.apellido = apellido
+
     def __str__(self):
         return f"({self.dni}) {self.nombre} {self.apellido}"
-    
-
 class Clientes:
+
+    # Lista de clientes
     lista = []
 
     @staticmethod
@@ -15,24 +17,24 @@ class Clientes:
         for cliente in Clientes.lista:
             if cliente.dni == dni:
                 return cliente
-            
+
     @staticmethod
     def crear(dni, nombre, apellido):
         cliente = Cliente(dni, nombre, apellido)
         Clientes.lista.append(cliente)
         return cliente
 
-
     @staticmethod
     def modificar(dni, nombre, apellido):
-        for indice, cliente in enumerate(Clientes.lista):
+        for i, cliente in enumerate(Clientes.lista):
             if cliente.dni == dni:
-                Clientes.lista[indice].nombre = nombre
-                Clientes.lista[indice].apellido = apellido
-                return Clientes.lista[indice]
+                Clientes.lista[i].nombre = nombre
+                Clientes.lista[i].apellido = apellido
+                return Clientes.lista[i]
 
     @staticmethod
     def borrar(dni):
-        for indice, cliente in enumerate(Clientes.lista):
+        for i, cliente in enumerate(Clientes.lista):
             if cliente.dni == dni:
-                return Clientes.lista.pop(indice)
+                cliente = Clientes.lista.pop(i)
+                return cliente
