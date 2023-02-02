13b62e5e1e6b1e1472c43a4076734085aa0e996a
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-02-02 08:43:21 -0600

    ui interfaz part 2

--- ui.py
+++ ui.py
@@ -5,6 +5,7 @@ class MainWindow(Tk):
         super().__init__()
         self.title('Gestor de Clientes')
         self.build()
+        self.center()
 
     def build(self):
         button = Button(self,text='Hola Mundo', command=self.hola)
@@ -12,6 +13,16 @@ class MainWindow(Tk):
 
     def hola(self):
         print('Hola Mundo')
+
+    def center(self):
+        self.update()
+        w = self.winfo_width()
+        h = self.winfo_height()
+        ws = self.winfo_screenwidth()
+        hs = self.winfo_screenheight()
+        x = int((ws/2) - (w/2))
+        y = int((hs/2) - (h/2))
+        self.geometry(f"{w}x{h}+{x}+{y}")
 
 if __name__ == '__main__':
     app = MainWindow()
