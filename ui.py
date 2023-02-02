5adffa5985bb70b2ab617d5fbfc28b6bb906c445
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-02-02 08:44:49 -0600

    ui interfaz part 3

--- ui.py
+++ ui.py
@@ -1,6 +1,19 @@
 from tkinter import *
 
-class MainWindow(Tk):
+
+class CenterWidgetMixin:
+    def center(self,):
+        self.update()
+        w = self.winfo_width()
+        h = self.winfo_height()
+        ws = self.winfo_screenwidth()
+        hs = self.winfo_screenheight()
+        x = int((ws/2) - (w/2))
+        y = int((hs/2) - (h/2))
+        self.geometry(f"{w}x{h}+{x}+{y}")
+
+
+class MainWindow(Tk, CenterWidgetMixin):
     def __init__(self):
         super().__init__()
         self.title('Gestor de Clientes')
@@ -14,15 +27,6 @@ class MainWindow(Tk):
     def hola(self):
         print('Hola Mundo')
 
-    def center(self):
-        self.update()
-        w = self.winfo_width()
-        h = self.winfo_height()
-        ws = self.winfo_screenwidth()
-        hs = self.winfo_screenheight()
-        x = int((ws/2) - (w/2))
-        y = int((hs/2) - (h/2))
-        self.geometry(f"{w}x{h}+{x}+{y}")
 
 if __name__ == '__main__':
     app = MainWindow()
